f2b2e816bea973b145dc6b3e3348991f58bdd93a
Rose Weixel <rose.weixel@gmail.com>
2014-09-25 12:39:22 -0400

    Update application layout

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,6 +2,12 @@ class VisitorsController < ApplicationController
 
 	def new
 		@owner = Owner.new
+		flash.now[:notice] = "Welcome! Today is #{Date.today}."
+		if @owner.countdown > 90
+			flash.now[:notice] << ' My next birthday is still far away.'
+		else
+			flash.now[:alert] = 'My birthday is soon.'
+		end
 	end
 	
 end
